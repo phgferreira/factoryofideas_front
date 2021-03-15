93a6896839a116c7eb21d09a97c57a2695c9a87d
Paulo Ferreira <phgferreira@outlook.com>
2021-03-15 16:35:26 -0300

    Envia os dados em post mas não para o back-end ainda

--- src/App.js
+++ src/App.js
@@ -1,44 +1,101 @@
+import React, { Component } from 'react';
 import './App.css';
-import { InputGroup, FormControl, Button } from 'react-bootstrap/';
+import { Form, Button } from 'react-bootstrap/';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const setores = [
-  {"id": "1", "name": "Tecnologia da Informação"},
-  {"id": "2", "name": "Controle de Infecção"},
-  {"id": "2", "name": "Nutição"},
-  {"id": "2", "name": "Faturamento"},
-  {"id": "2", "name": "Gestão da Qualidade"}
+const sectors = [
+  'Escolha um setor',
+  'Tecnologia da Informação',
+  'Controle de Infecção',
+  'Nutição',
+  'Faturamento',
+  'Gestão da Qualidade'
 ]
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="Frame">
-          <InputGroup className="mb-3 First-Field">
-            <FormControl placeholder="Nome"/>
-          </InputGroup>
+  constructor(props) {
+    super(props);
 
-          <InputGroup className="mb-3 Other-Field">
-            <FormControl placeholder="Sujestão ..." as="textarea" rows="10"/>
-          </InputGroup>
+    this.state = {
+      name: '',
+      suggestion: '',
+      sector: ''
+    }
 
-          <InputGroup className="mb-3 Other-Field">
-            <FormControl placeholder="Setor" as="select">
-              {
-                setores.map(setor => (
-                  <option value={setor.id}>{setor.name}</option>
-                ))
-              }
-            </FormControl>
-          </InputGroup>
+    this.onChange = this.onChange.bind(this);
+  }
 
-          <Button className="Other-Field" variant="primary" size="lg">Primary</Button>
-        </div>
-      </header>
-    </div>
-  );
+  onChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  async postIdea() {
+    try {
+      let result = await fetch('https://webhook.site/68e70d52-1c5b-4448-9e26-06d1f08a8a4a', {
+        method: 'post',
+        mode: 'no-cors',
+        headers: {
+            
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: this.state.name,
+          suggestion: this.state.suggestion,
+          sector: this.state.sector
+        })
+      })
+
+      console.log('Sucess: ' + result);
+
+    } catch(e) {
+      console.error('Failed: ' + e);
+    }
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <div className="Frame">
+            <Form.Group className="Field First-Field">
+              <Form.Label>Nome (opcional)</Form.Label>
+              <Form.Control name="name" placeholder="Digite seu nome"
+                value={ this.state.name }
+                onChange={ this.onChange }></Form.Control>
+            </Form.Group>
+
+            <Form.Group className="Field">
+              <Form.Label>Sugestão</Form.Label>
+              <Form.Control name="suggestion" as="textarea" placeholder="Digite aqui sua sujestão" rows="10"
+                value={ this.state.suggestion }
+                onChange={ this.onChange }></Form.Control>
+            </Form.Group>
+
+            <Form.Group className="Field">
+              <Form.Label>Setor</Form.Label>
+              <Form.Control name="sector" as="select"
+                value={ this.state.sector }
+                onChange={ this.onChange }>
+                {
+                  sectors
+                    .sort( (a, b) => a > b ? 1 : -1 )
+                    .map( sector => (
+                      <option key={sector} value={sector}> {sector} </option>
+                    ))
+                }
+              </Form.Control>
+            </Form.Group>
+
+            <Button onClick={ () => this.postIdea() } className="Send-Button" variant="primary" size="lg">Enviar</Button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

a2ec1fc7ca834253d0d1b4eebfc94e6d18e3176b
Paulo Ferreira <phgferreira@outlook.com>
2021-03-18 18:16:19 -0300

    Mensagem de erro configuradas

--- src/App.js
+++ src/App.js
@@ -7,35 +7,29 @@ import IdeaService from './services/IdeaService';
 
 class App extends Component {
 
-  initialState = {
-    name: '',
-    suggestion: '',
-    sector: {
-      id: ''
+  resetState = {
+    idea: {
+      name: '',
+      contact: '',
+      suggestion: '',
+      sector: {
+        id: 0
+      }
     },
-    sectors: [],
-    result: 'ainda não foi enviado',
-    variant: 'secondary'
+
+    suggestionRequired: false,
+    sectorRequired: false,
+
   }
 
   constructor(props) {
     super(props);
-
-    this.state = this.initialState;
-
-    this.onChange = this.onChange.bind(this);
-  }
-
-  reset() {
-    this.setState(this.initialState);
-    this.componentDidMount();
-    this.setState({ result: true })
-  }
-
-  onChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+    this.state = { ...this.resetState,
+      sectors: [],
+      message: false,
+      variant: 'secondary',
+      disabled: false
+    };
   }
 
   componentDidMount() {
@@ -46,15 +40,24 @@ class App extends Component {
     })
   }
 
+  reset() {
+    this.setState(this.initialState);
+    this.componentDidMount();
+  }
+
   async postIdea() {
     try {
-      IdeaService.sendEmail(this.state).then((response) => {
-        this.reset();
-        this.setState({
-          result: response.data,
-          variant: 'success'
-        })
-      });
+      if (this.state.idea.suggestion === '') {
+        this.setState({ suggestionRequired: 'A sugestão é obrigatória' })
+      } else if (this.state.idea.sector.id === 0) {
+        this.setState({ sectorRequired: 'O setor é obrigatório' })
+      } else {
+        this.setState({ message: 'Enviando ...', variant: 'warning', suggestionRequired: false, sectorRequired: false, disabled: true });
+        IdeaService.sendEmail(this.state.idea).then((response) => {
+          this.reset();
+          this.setState({ message: 'Enviado com sucesso', variant: 'success' })
+        });
+      }
     } catch(e) {
       console.error('Failed: ' + e);
     }
@@ -65,37 +68,69 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <div className="Frame">
+
             <Form.Group className="Field First-Field">
               <Form.Label>Nome (opcional)</Form.Label>
               <Form.Control name="name" placeholder="Digite seu nome"
-                value={ this.state.name }
-                onChange={ this.onChange }></Form.Control>
+                value={ this.state.idea.name }
+                onChange={ (e) => { this.setState({ idea: { 
+                  name: e.target.value,
+                  contact: this.state.idea.contact,
+                  suggestion: this.state.idea.suggestion,
+                  sector: this.state.idea.sector
+                 } }) } }></Form.Control>
             </Form.Group>
 
             <Form.Group className="Field">
-              <Form.Label>Sugestão</Form.Label>
-              <Form.Control name="suggestion" as="textarea" placeholder="Digite aqui sua sujestão" rows="10"
-                value={ this.state.suggestion }
-                onChange={ this.onChange }></Form.Control>
+              <Form.Label>Contato (opcional)</Form.Label>
+              <Form.Control name="contact" placeholder="Digite um contato para retorno"
+                value={ this.state.idea.contact }
+                onChange={ (e) => { this.setState({ idea: {
+                  name: this.state.idea.name,
+                  contact: e.target.value,
+                  suggestion: this.state.idea.suggestion,
+                  sector: this.state.idea.sector
+                } }) } }></Form.Control>
             </Form.Group>
 
             <Form.Group className="Field">
-              <Form.Label>Setor</Form.Label>
+              <Form.Label>Sugestão*</Form.Label>
+              <Form.Control name="suggestion" as="textarea" placeholder="Digite aqui sua sujestão" rows="5"
+                value={ this.state.idea.suggestion }
+                onChange={ (e) => { this.setState({ idea: {
+                  name: this.state.idea.name,
+                  contact: this.state.idea.contact,
+                  suggestion: e.target.value,
+                  sector: this.state.idea.sector
+                } }) } }></Form.Control>
+              <Alert show={ this.state.suggestionRequired } name="suggestionRequired" variant="danger">{ this.state.suggestionRequired }</Alert>
+            </Form.Group>
+
+            <Form.Group className="Field">
+              <Form.Label>Setor*</Form.Label>
                 <Form.Control name="sector" as="select"
-                    onChange={ (e) => { this.setState({ sector: { id: e.target.value } }) } }>
+                    onChange={ (e) => { this.setState({ idea: {
+                      name: this.state.idea.name,
+                      contact: this.state.idea.contact,
+                      suggestion: this.state.idea.suggestion,
+                      sector: { id: e.target.value }
+                    } }) } }>
+                      <option key="0" value="0">Escolha o setor</option>
                     {
                     this.state.sectors
                         .sort( (a, b) => a.name > b.name ? 1 : -1 )
                         .map( sector => (
-                        <option key={sector.id} value={sector.id}> {sector.name} </option>
+                          <option key={sector.id} value={sector.id}> {sector.name} </option>
                         ))
                     }
                 </Form.Control>
+                <Alert show={ this.state.sectorRequired } name="sectorRequired" variant="danger">{ this.state.sectorRequired }</Alert>
             </Form.Group>
 
-            <Button onClick={ () => this.postIdea() } className="Send-Button" variant="primary" size="lg">Enviar</Button>
+            <Button className="Send-Button" variant="primary" size="lg"
+              disabled={ this.state.disabled } onClick={ () => this.postIdea() }>Enviar</Button>
 
-            <Alert variant={ this.state.variant }>{ this.state.result }</Alert>
+            <Alert show={ this.state.message } variant={ this.state.variant }>{ this.state.message }</Alert>
 
           </div>
         </header>
